b223cb12e80b22e7b0953264fb7e0ee029449bfd
jcyang <34616545+KevinBastianYang@users.noreply.github.com>
2018-10-05 20:32:33 +0800

    revised

--- Kallisto/kallisto_map_all.sh
+++
@@ -1,3 +0,0 @@
-export PATH=$PATH:/home/yangjc/summer_proj/kallisto_linux-v0.44.0
-
-kallisto pseudo -i ./transcriptome/transcripts.idx -o ./TCC_output_all/ --umi -b /home/yangjc/summer_proj/allcells/E31_TCC/new_umi_read_list.txt -t 8
